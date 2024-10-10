5dffb6abfd93a1e0d0f7fd8b33b33f357abea9df
Guilherme220501 <guilherme8876sk@gmail.com>
2024-10-10 13:02:58 -0300

    bhbvgv

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function toggleMenu() {
         sidebar.style.left = '-250px'; // Fecha o menu
     }
 }
+function showTime() {
+    const clock = document.getElementById("clock");
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    clock.textContent =  `Hora: 
+    ${hours}:${minutes}:${seconds}`;
+}
+setInterval(showTime, 1000);
